8511eab96a6b487263feb7473e21835f2e581339
Tiago <tiago.gomes@ifsp.edu.br>
2017-03-08 02:44:57 -0300

    Oops

--- sync.py
+++ sync.py
@@ -13,13 +13,13 @@
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# ldap3 is distributed in the hope that it will be useful,
+# RADIUSSyncServer is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with ldap3 in the COPYING and COPYING.LESSER files.
+# along with RADIUSSyncServer in the COPYING and COPYING.LESSER files.
 # If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import unicode_literals
